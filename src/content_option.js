2da55387f3bdc8688a87b075545d09e62d45be64
alla0810 <kyosook.shin@gmail.com>
2024-02-13 20:15:00 -0600

    initial update

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "KyoSook";
+const meta = {
+    title: "KyoSook Shin",
+    description: "I’m KyoSook Shin Embedded Software Enginner, Full-Stack Developer, Front-End Developer, Back-End Developer, currently available in Austin",
+};
+
+const introdata = {
+    title: "I’m KyoSook Shin",
+    animated: {
+        first: "I love software",        
+        second: "Embedded Software Engineer",
+        third: "Full-Stack Developer",
+    },
+    description: "software engineer with 10+ years of experience",
+    your_img_url: "https://rms.koenig-solutions.com/Sync_data/CCE_Logo/2341-SoftwareDeveloperJobDescription.jpegL.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hi, my name is KyoSook Shin.  I came to the US in 2017 from South Korea. I've been away from professional arena to raise my child, but my mind has been always there.  After long preparation, now I'm ready to jump and work for you.",
+    work: "Embedded Software Engineer | Full-stack Developer | Back-End Develper | Front-End Developer",
+};
+const worktimeline = [{
+        jobtitle: "Dicom PACS Software Development",
+        where: "Dimedi Korea",
+        date: "June 2013 - Aug 2014",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kyosook.shin@gmail.com",
+//    YOUR_FONE: "(515)844-4068",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_msla5c9",
+    YOUR_TEMPLATE_ID: "template_fc0hc5a",
+    YOUR_USER_ID: "JEmp-25WPNMthnPuy",
+};
+
+const socialprofils = {
+    email: "mailto:kyosook.shin@gmail.com",    
+    github: "https://github.com/alla0810",
+    linkedin: "https://www.linkedin.com/in/kyosook-shin/",
+    gist: "https://gist.github.com/alla0810",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
